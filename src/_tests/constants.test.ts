93f829f43c2d8a0baa70fdab58639f5319b40aa0
changshou83 <1369548156@qq.com>
2022-11-13 19:49:23 +0800

    test: add BASE_URL_CN test

--- src/_tests/constants.test.ts
+++ src/_tests/constants.test.ts
@@ -1,8 +1,12 @@
-import { BASE_URL, USER_AGENT } from "../constants";
+import { BASE_URL, BASE_URL_CN, USER_AGENT } from "../constants";
 
 describe("Contants", () => {
     test("BASE_URL", () => {
         expect(BASE_URL).toBe("https://leetcode.com");
+    });
+
+    test("BASE_URL_CN", () => {
+        expect(BASE_URL_CN).toBe("https://leetcode.cn");
     });
 
     test("USER_AGENT", () => {
